a102ff74f9a89c3ccdd91f807601f03b1f7e0278
Muzakkir Hussain <141902182+muzakkir27sept@users.noreply.github.com>
2024-08-19 21:58:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,33 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_ecs_cluster" "notification_cluster" {
+  name = "notification-cluster"
+}
+
+# Create an ECR repository for the Notification API
+resource "aws_ecr_repository" "notification_api" {
+  name = "notification-api"
+}
+
+# Create an ECR repository for the Email Sender service
+resource "aws_ecr_repository" "email_sender" {
+  name = "email-sender"
+}
+
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
 }
