8d78f9c6dd6d8dc0cb85ae3ddf73b1a9269ca354
Muzakkir Hussain <141902182+muzakkir27sept@users.noreply.github.com>
2024-08-19 22:01:18 +0530

    Update secrets-manager.tf

--- terraform/secrets-manager.tf
+++ terraform/secrets-manager.tf
@@ -1,3 +1,16 @@
+
 resource "aws_secretsmanager_secret" "email_service_secret" {
-  name = "email-service-credentials"
+  name        = "email-service-credentials"
+  description = "Stores the email service API credentials"
+  recovery_window_in_days = 7
+}
+
+
+resource "aws_secretsmanager_secret_version" "email_service_secret_version" {
+  secret_id = aws_secretsmanager_secret.email_service_secret.id
+
+  secret_string = jsonencode({
+    username = "your-email-service-username",  
+    password = "your-email-service-password"   
+  })
 }
